76acf83cf8630d0e6206347a6cfb0ce07ecfd8ec
pboymt <10893582+pboymt@users.noreply.github.com>
2023-04-08 21:28:31 +0800

    fix: 优化右侧面板中画面尺寸

--- template-creator/src/components/NewPanel.vue
+++ template-creator/src/components/NewPanel.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="panel">
+        <!-- <div class="mouse-position">
+            <div>{{ mouseActived ? '点击' : '抬起' }}</div>
+            <div>{{ mousePosX }}</div>
+            <div>{{ mousePosY }}</div>
+        </div> -->
         <div class="background" ref="bgDiv"></div>
         <!-- <canvas ref="rBgCanvasElement" width="100" height="100"></canvas> -->
-        <canvas ref="rCanvasElement" width="100" height="100"></canvas>
+        <canvas ref="rCanvasElement" width="100" height="100" @mousemove="onMouseMove"
+            @mousedown="onMouseUpDown($event, false)" @mouseup="onMouseUpDown($event, true)"
+            @mouseout="onMouseUpDown($event, true)" @contextmenu="$event.preventDefault()"></canvas>
     </div>
 </template>
 
@@ -23,6 +30,10 @@ const rCanvasElement = ref<HTMLCanvasElement>();
 let mCtx: CanvasRenderingContext2D | null = null;
 let count = 0;
 
+const mouseActived = ref(false);
+const mousePosX = ref(0);
+const mousePosY = ref(0);
+
 window.myWorker.addEventListener('message', (e) => {
     // console.log('message', e.data);
     const bitmap: ImageBitmap = e.data;
@@ -32,7 +43,21 @@ window.myWorker.addEventListener('message', (e) => {
     count++;
 });
 
+function onMouseMove(e: MouseEvent) {
+    // const { offsetX, offsetY } = e;
+    // console.log(e.buttons);
+    // const x = Math.round(rCanvasElement.value!.width * offsetX / rCanvasElement.value!.offsetWidth);
+    // const y = Math.round(rCanvasElement.value!.height * offsetY / rCanvasElement.value!.offsetHeight);
+    // for (const roi of project?.value?.rois.values() ?? []) {
+    //     if (roi.rect.bounds.contains(x, y)) {
+    //         console.log(roi.name);
+    //     }
+    // }
+}
 
+function onMouseUpDown(e: MouseEvent, up: boolean) {
+    mouseActived.value = !up;
+}
 
 function initDrawable() {
     if (project?.value) {
@@ -99,12 +124,22 @@ div.panel {
     align-items: center;
     justify-content: center;
 
+    div.mouse-position {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 100;
+        background-color: rgba(0, 0, 0, 0.5);
+        color: white;
+        padding: 5px;
+    }
+
     canvas {
         position: absolute;
         display: block;
         flex: none;
-        max-width: 100%;
-        max-height: 100%;
+        max-width: 98%;
+        max-height: 98%;
         background-color: transparent;
     }
 
@@ -112,8 +147,8 @@ div.panel {
         position: absolute;
         display: block;
         flex: none;
-        max-width: 100%;
-        max-height: 100%;
+        max-width: 98%;
+        max-height: 98%;
         background-color: transparent;
         background-size: contain;
     }
@@ -122,8 +157,8 @@ div.panel {
         position: relative;
 
         img {
-            max-width: 100%;
-            max-height: 100%;
+            max-width: 99%;
+            max-height: 99%;
             margin-left: 1px;
         }
 
