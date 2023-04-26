da03478e48dd4b19d6ab64844f8f2f2d826c34e6
pboymt <10893582+pboymt@users.noreply.github.com>
2023-04-26 16:35:59 +0800

    chore: 调整代码格式并移除部分未使用导入

--- template-creator/src/about.ts
+++ template-creator/src/about.ts
@@ -6,21 +6,3 @@ import 'material-symbols';
 import { getCurrent, LogicalSize } from '@tauri-apps/api/window';
 
 createApp(App).mount("#app");
-// import { getCurrent, LogicalSize, PhysicalSize } from '@tauri-apps/api/window';
-
-// console.log('a');
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('b');
-//     document.getElementById('close-window')?.addEventListener('click', () => {
-//         console.log('c');
-//         getCurrent().hide();
-//     });
-// });
-
-// window.addEventListener('resize', () => {
-//     const { innerHeight, innerWidth } = window;
-//     if (innerHeight > 400 || innerWidth > 500) {
-//         getCurrent().setSize(new LogicalSize(500, 400));
-//     }
-// });
-
--- template-creator/src/export-image.ts
+++ template-creator/src/export-image.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
-import "./style.scss";
-import App from "./Export.vue";
+import "@/style.scss";
+import App from "@/Export.vue";
 import 'material-symbols';
-import { getCurrent, LogicalSize } from '@tauri-apps/api/window';
 
 createApp(App).mount("#app");
